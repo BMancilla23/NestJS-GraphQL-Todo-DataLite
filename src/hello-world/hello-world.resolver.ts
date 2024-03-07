61e5698c23dc9ef4331d720a8e65ad77f5664335
bmancilla23 <bmancilla119@gmail.com>
2024-03-07 07:46:50 -0500

    Implementación de Querys

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,10 +1,23 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
 
-    @Query(() => String)
+    @Query(() => String, {description: 'Hola Mundo es lo que retorna', name: 'hello'})
     hellowWorld(): string {
         return 'Hola Mundo'
     }
+
+    @Query(() => Float, {name: 'randomNumber'})
+    getRandomNumber(): Number{
+        return Math.random() * 100;
+    }
+
+    // randomFromZeroTo
+    @Query(() => Int, {name: 'randomFromZeroTo'})
+    getRandomFromZeroTo(): number{
+        return Math.floor(Math.random() * 10);  // int
+    }
 }
+
+
